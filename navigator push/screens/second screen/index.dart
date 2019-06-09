972a55c722dab5b066b4d97a332385212fe158ba
Aditi <aditi@Aditis-Air.Dlink>
2019-06-09 11:19:38 +0530

    first commit

---
+++ navigator push/screens/second screen/index.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        onPressed: (){
+          print("floating action");
+        },
+        child: Icon(Icons.edit),
+      ),
+      appBar: AppBar(
+        title: Text("Unread", style: TextStyle( fontSize: 30.0),),
+        centerTitle: false,
+        backgroundColor: Colors.red,
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.white,),
+          onPressed: () {},
+        ),
+        
+        actions: <Widget>[
+       IconButton(
+         icon: Icon(Icons.search),
+         onPressed: (){
+           print("search");
+         },
+       )
+        ],   
+      
+    ),
+    body: ListView(
+      shrinkWrap: true,
+      
+      children:ListTile.divideTiles(
+        context: context,
+        tiles:<Widget>[
+        ListTile(
+          
+          leading: Container(
+            height: 250,
+            width: 50,
+            child: Icon(Icons.fiber_manual_record,size: 50),
+            
+          ),
+          trailing: Container(
+            width: 320,
+            height: 250,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                   Text("Amazon Alexa Team", style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),) ,
+                    Text("10:57AM", style: TextStyle( fontStyle: FontStyle.normal,fontSize: 12,color: Colors.blue)),
+                   
+                  ],
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("[REMIND...]This october build Alexa skills a..",style: TextStyle( fontSize: 14, fontWeight: FontWeight.bold)),
+                  ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                   Text("Build alexa skills;get Sony Play Station 4,Bose..", style: TextStyle(fontSize: 12.0),) ,
+                    Icon(Icons.star, size: 18,),
+                   
+                  ],
+              
+                ),
+                
+              ],
+            ),
+          ),
+        ),
+        ListTile(
+          
+          leading: Container(
+            height: 350,
+            width: 50,
+            child: Icon(Icons.fiber_manual_record,size: 50),
+            
+          ),
+          trailing: Container(
+            width: 320,
+            height: 350,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                   Text("Amazon Alexa Team", style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),) ,
+                    Text("10:57AM", style: TextStyle( fontStyle: FontStyle.normal,fontSize: 12,color: Colors.blue)),
+                   
+                  ],
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("[REMIND...]This october build Alexa skills a..",style: TextStyle( fontSize: 14, fontWeight: FontWeight.bold)),
+                  ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                   Text("Build alexa skills;get Sony Play Station 4,Bose..", style: TextStyle(fontSize: 12.0),) ,
+                    Icon(Icons.star, size: 18,),
+                   
+                  ],
+                ),
+                
+              ],
+            ),
+          ),
+        ),
+        
+        
+      ],
+    ).toList(),
+    ),
+    );
+    }
+}
